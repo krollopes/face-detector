e70c41b854d7119592485e4dcaef068b4aa4b791
Caroline Lopes <kaka201155@gmail.com>
2025-02-24 14:42:47 -0300

    feat: add navigation link to logs page

--- src/components/VideoStream.tsx
+++ src/components/VideoStream.tsx
@@ -3,7 +3,7 @@
 import { useFaceDetection } from '@/hooks/useFaceDetection';
 
 const VideoStream = () => {
-  const { videoRef, canvasRef, loading } = useFaceDetection();
+  const { videoRef, canvasRef, faceCount, loading } = useFaceDetection();
 
   return (
     <div className='relative w-full max-w-lg mx-auto'>
@@ -15,6 +15,11 @@ const VideoStream = () => {
         className='w-full rounded-lg shadow-lg'
       />
       <canvas ref={canvasRef} className='absolute top-0 left-0 w-full h-full' />
+
+      {/* Exibir contagem de rostos */}
+      <div className='absolute top-2 right-2 bg-black text-white px-4 py-2 rounded-lg shadow-md'>
+        Faces Detected: {faceCount}
+      </div>
     </div>
   );
 };
